8d6ece5d556acc201bbe1e155542e8cefff7893f
Truptigajera <gajeratupi@gmail.com>
2023-09-26 23:36:32 +0530

    26/09/2023

---
+++ output/static_method.cpp
@@ -0,0 +1,30 @@
+//static variable & method
+#include<iostream>
+using namespace std;
+class Test{
+    int carRating;
+    public:
+    static int num; //declare
+    void setRating(){
+        cout<<"Enter car Rating:";
+        cin>>carRating;
+        num++;
+    }
+    void display(){
+        cout<<"your rating is:"<<carRating<<endl;
+    }
+    static int number(){
+        return num;
+    }
+
+};
+int Test :: num=15;
+int main(){
+    cout<<"strating stage:"<<Test :: number()<<endl;
+    Test t1,t2,t3;
+    t1.setRating();
+    t1.display();
+    t2.setRating();
+    t2.display();
+    cout<<"Ending stage:"<<Test :: number()<<endl;
+}
---
+++ output/template.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+template<class T>
+/*void display(T t1)
+{
+    cout<<"Displaying Tamplate :"<<t1<<endl;
+}
+*/
+template<class x,class y>
+//void Add(x a, y b)
+
+ //cout<<"Addition is:"<<a+b<<endl;
+
+void Display(x a, y b)
+{cout<<"Displaying Template:"<<a<<"\t"<<b<<endl;}
+int main()
+{
+   // display(200);
+    //display(12.68);
+    //display('G');
+ //   Display('G',1.25);
+    Display('x',25);
+    Display(25,1.25);
+    //Add(2,5.25);
+    //Add('a',5.25);
+    return 0;
+
+}
